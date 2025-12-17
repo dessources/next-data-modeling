1499d38d3f51c95c558f9add5bd85cbebe4c096f
Jaem Dessources Dit Dumesle <jaem.developer@gmail.com>
2025-12-17 16:51:09 -0500

    Ignored env files

--- src/app/exercises/client-pool/page.exercise.tsx
+++ src/app/exercises/client-pool/page.exercise.tsx
@@ -1,40 +1,28 @@
 // 🐶 Importe le Type `Todo` il nous sera utile pour typer les données reçues
-// import {Todo} from '@/lib/type'
+import {Todo} from '@/lib/type'
 
 // 🐶 Importe la classe `Client` elle nous permettra d'instancier un client `postgres`
-// import {Client} from 'pg'
+import {Client} from 'pg'
 
 async function getTodos() {
-  // 🐶 Crée une instance de `Client` avec les informations de connexion
+  const client = new Client({
+    connectionString: process.env.POSTGRES_URL_LOCAL,
+  })
 
-  // Utilise `connectionString` pour se connecter à la base de données
-  // `connectionString` est une chaine de caractère qui contient les informations de connexion
-  // Forme : 'postgres://<user>:<password>@localhost:5432/<bdd_name>?sslmode=disable'
-  // Exemple : 'postgres://postgres:admin@localhost:5432/postgres?sslmode=disable'
-  // 📑 doc : https://node-postgres.com/apis/client
+  const db = await client.connect()
 
-  // 🤖 const client = new Client({...
-
-  // 🐶 Connecte toi à la base de données
-  // 📑 doc : https://node-postgres.com/features/connecting#environment-variables
-
-  // 🤖 await client.connect()
-
-  // 🐶 Récupère les données de la table `Todo` avec la requête SQL suivante
-  // 🤖 `SELECT id, title, iscompleted AS "isCompleted",  createdat AS "createdAt",  updatedat AS "updatedAt" from Todo`
-
-  // 🐶 Pour exécuter cette requête :
-  // 🤖 const {rows} = await client.query<Todo>(`SELECT ...`)
-
-  const rows = [
-    {
-      id: 1,
-      title: 'Fake Data',
-      isCompleted: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ]
+  const {rows} = await client.query<Todo>(
+    `SELECT id, title, iscompleted AS "isCompleted",  createdat AS "createdAt",  updatedat AS "updatedAt" from Todo`
+  )
+  // const rows = [
+  //   {
+  //     id: 1,
+  //     title: 'Fake Data',
+  //     isCompleted: false,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // ]
   // 🐶 Déconnecte toi de la base de données avec
   // 🤖 await client.end()
   return rows
@@ -45,7 +33,7 @@ export default async function Page() {
   return (
     <div className="mx-auto max-w-2xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold">Todo List</h1>
-      {rows.map((todo) => (
+      {rows.map((todo: Todo) => (
         <div key={todo.id}>
           <input
             type="checkbox"
